d7b67fad692bbc9c22b377549c003313c6b4144e
Erumebor Lucky <lucky@creativecodez.com>
2016-08-19 21:10:09 +0100

    Update openfb.js

--- www/openfb.js
+++ www/openfb.js
@@ -1,15 +1,8 @@
 module.exports = {
-    greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
-    },
-    alert: function(){
-        alert('Hello');
-    }
-};
-var openFB = (function () {
+  var openFB = (function () {
     var loginURL = 'https://www.facebook.com/dialog/oauth',
         logoutURL = 'https://www.facebook.com/logout.php',
-        tokenStore = window.localStorage,
+        tokenStore = window.sessionStorage,
         fbAppId,
         context = window.location.pathname.substring(0, window.location.pathname.lastIndexOf("/")),
         baseURL = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + context,
@@ -23,22 +16,27 @@ var openFB = (function () {
         runningInCordova = true;
     }, false);
     function init(params) {
+
         if (params.appId) {
             fbAppId = params.appId;
         } else {
             throw 'appId parameter not set in init()';
         }
+
         if (params.tokenStore) {
             tokenStore = params.tokenStore;
         }
+
         if (params.accessToken) {
             tokenStore.fbAccessToken = params.accessToken;
         }
+
         loginURL = params.loginURL || loginURL;
         logoutURL = params.logoutURL || logoutURL;
         oauthRedirectURL = params.oauthRedirectURL || oauthRedirectURL;
         cordovaOAuthRedirectURL = params.cordovaOAuthRedirectURL || cordovaOAuthRedirectURL;
         logoutRedirectURL = params.logoutRedirectURL || logoutRedirectURL;
+
     }
     function getLoginStatus(callback) {
         var token = tokenStore.fbAccessToken,
@@ -52,6 +50,7 @@ var openFB = (function () {
         if (callback) callback(loginStatus);
     }
     function login(callback, options) {
+
         var loginWindow,
             startTime,
             scope = '',
@@ -72,22 +71,24 @@ var openFB = (function () {
         }
         function loginWindow_exitHandler() {
             console.log('exit and remove listeners');
-            // Handle the situation where the user closes the login window manually before completing the login process
             if (loginCallback && !loginProcessed) loginCallback({status: 'user_cancelled'});
             loginWindow.removeEventListener('loadstop', loginWindow_loadStopHandler);
             loginWindow.removeEventListener('exit', loginWindow_exitHandler);
             loginWindow = null;
             console.log('done removing listeners');
         }
+
         if (options && options.scope) {
             scope = options.scope;
         }
+
         loginCallback = callback;
         loginProcessed = false;
+
         startTime = new Date().getTime();
         loginWindow = window.open(loginURL + '?client_id=' + fbAppId + '&redirect_uri=' + redirectURL +
             '&response_type=token&scope=' + scope, '_blank', 'location=no,clearcache=yes');
-       if (runningInCordova) {
+        if (runningInCordova) {
             loginWindow.addEventListener('loadstart', loginWindow_loadStartHandler);
             loginWindow.addEventListener('exit', loginWindow_exitHandler);
         }
@@ -95,6 +96,7 @@ var openFB = (function () {
     function oauthCallback(url) {
         var queryString,
             obj;
+
         loginProcessed = true;
         if (url.indexOf("access_token=") > 0) {
             queryString = url.substr(url.indexOf('#') + 1);
@@ -112,7 +114,7 @@ var openFB = (function () {
     function logout(callback) {
         var logoutWindow,
             token = tokenStore.fbAccessToken;
-        tokenStore.removeItem('fbtoken');
+            tokenStore.removeItem('fbtoken');
         if (token) {
             logoutWindow = window.open(logoutURL + '?access_token=' + token + '&next=' + logoutRedirectURL, '_blank', 'location=no,clearcache=yes');
             if (runningInCordova) {
@@ -121,17 +123,49 @@ var openFB = (function () {
                 }, 700);
             }
         }
+
         if (callback) {
             callback();
         }
+
     }
-   function api(obj) {
+    function img(obj) {
+
         var method = obj.method || 'GET',
             params = obj.params || {},
             xhr = new XMLHttpRequest(),
             url;
+
         params['access_token'] = tokenStore.fbAccessToken;
-        url = 'https://graph.facebook.com' + obj.path + '?' + toQueryString(params);
+
+        url = 'https://graph.facebook.com/v2.7' + obj.path + '?' + toQueryString(params) +'&fields=id,birthday,email,first_name,gender,last_name,link,locale,name,timezone,updated_time,verified,website&format=json';
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    if (obj.success) obj.success(xhr.responseURL);
+                } else {
+                    var error = xhr.responseText ? JSON.parse(xhr.responseText).error : {message: 'An error has occurred'};
+                    if (obj.error) obj.error(error);
+                }
+            }
+        };
+
+        xhr.open(method, url, true);
+        xhr.send();
+    }
+
+    function api(obj) {
+
+        var method = obj.method || 'GET',
+            params = obj.params || {},
+            xhr = new XMLHttpRequest(),
+            url;
+
+        params['access_token'] = tokenStore.fbAccessToken;
+
+        url = 'https://graph.facebook.com/v2.7' + obj.path + '?' + toQueryString(params) +'&fields=id,birthday,email,first_name,gender,last_name,link,locale,name,timezone,updated_time,verified,website&format=json';
+
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
@@ -142,10 +176,11 @@ var openFB = (function () {
                 }
             }
         };
+
         xhr.open(method, url, true);
         xhr.send();
     }
-  function revokePermissions(success, error) {
+    function revokePermissions(success, error) {
         return api({method: 'DELETE',
             path: '/me/permissions',
             success: function () {
@@ -153,6 +188,7 @@ var openFB = (function () {
             },
             error: error});
     }
+
     function parseQueryString(queryString) {
         var qs = decodeURIComponent(queryString),
             obj = {},
@@ -163,6 +199,7 @@ var openFB = (function () {
         });
         return obj;
     }
+
     function toQueryString(obj) {
         var parts = [];
         for (var i in obj) {
@@ -172,13 +209,17 @@ var openFB = (function () {
         }
         return parts.join("&");
     }
+
     return {
         init: init,
         login: login,
         logout: logout,
         revokePermissions: revokePermissions,
         api: api,
+        img: img,
         oauthCallback: oauthCallback,
         getLoginStatus: getLoginStatus
     }
-}());
+
+}());  
+};
